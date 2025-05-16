95fdb76d2d10ca2c0049bcf462f88bb1baea362b
Soroush Kasra <211555874+soroushksra0x@users.noreply.github.com>
2025-05-16 15:33:37 +0430

    Set up React Router with routes for Home and Login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 // import "./pages/Home/Home.jsx"; // Assuming you have a CSS file for styling
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login";
 const App = () => {
   return (
     <div>
-      <Home />
+      {/* <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login /> }/>
+      </Routes> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
